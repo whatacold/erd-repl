abc539c5e4778e530fb520d00ffbd805533f1b0c
whatacold <whatacold@gmail.com>
2019-06-26 23:19:33 +0800

    Keep URI clean by rendering template on backend and storing session in cookie.

--- erd.py
+++ erd.py
@@ -6,11 +6,15 @@ import subprocess
 import json
 import random
 import time
+import sys
+sys.path.insert(0, "./submodules/")
+from PythonUtils import StringUtil
 
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, session
 from flask import render_template
 
 app = Flask(__name__)
+app.secret_key = b"B\x9fT\x80/\xf1'\xda9\x1f\xa4\xec>0\x8c-" # os.urandom(16)
 
 default_erd_source = '''
 [Person]
@@ -35,8 +39,9 @@ def gen_image(source_code, image_path):
                             #stdout=subprocess.PIPE
     )
     pobj.stdin.write(source_code.encode('utf-8'))
-    pobj.stdin.close()
-    pobj.communicate() # XXX
+    #pobj.stdin.close()
+    pobj.communicate(timeout=1) # XXX Need this to wait subprocess to terminate
+    app.logger.debug("popen return code: {}".format(pobj.returncode)) # TODO handle error
     return
     response = app.make_response(pobj.stdout.read())
     response.headers.set('Content-Type', 'image/png')
@@ -44,25 +49,37 @@ def gen_image(source_code, image_path):
 
 @app.route('/')
 def index():
-    return redirect('/static/erd-repl.html')
+    userid = ""
+    if "userid" in session:
+        userid = session["userid"]
+        app.logger.debug("cookie userid: %s", userid)
+    if len(userid) == 0:
+        userid = StringUtil.random_string()
+        session["userid"] = userid
+        app.logger.debug("set userid in cookie: %s", userid)
+    return render_template('erd-repl.html', userid=userid)
 
-@app.route('/erds/<id>', methods=['GET', 'PUT'])
-def erd(id):
+@app.route('/erds/<userid>', methods=['GET', 'PUT'])
+def erd(userid):
     if "PUT" == request.method:
-        obj = request.get_json() # TODO how to print debug log?
+        obj = request.get_json()
         imageFileName = obj["id"] + ".png"
-        path = "./static/erd-images/" + imageFileName # TODO
+        path = "./static/erd-images/" + imageFileName
         gen_image(obj["sourceCode"], path)
+        session["sourceCode"] = obj["sourceCode"]
 
         returnObj = {
-            "imageUri": '/static/erd-images/' + imageFileName + "?t=" + str(time.time()), # TODO
+            "imageUri": '/static/erd-images/' + imageFileName + "?t=" + str(time.time()),
         }
         return json.dumps(returnObj)
     else:
+        sourceCode = default_erd_source
+        if "sourceCode" in session:
+            sourceCode = session["sourceCode"]
         obj = {
-            "id": id,
-            "sourceCode": default_erd_source,
-            "imageUri": "/static/erd-images/" + id + ".png", # TODO
+            "id": userid,
+            "sourceCode": sourceCode,
+            "imageUri": "/static/erd-images/" + userid + ".png" + "?t=" + str(time.time()),
         }
         return json.dumps(obj)
 
