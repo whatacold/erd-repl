01eeba1755745c7122e368b3f4b2cc5ad5cacb29
whatacold <whatacold@gmail.com>
2019-07-04 21:12:52 +0800

    Extract config into config.py .

--- erd.py
+++ erd.py
@@ -13,105 +13,13 @@ from flask import Flask, redirect, request, session
 from flask import render_template, jsonify
 
 app = Flask(__name__)
-app.secret_key = b"B\x9fT\x80/\xf1'\xda9\x1f\xa4\xec>0\x8c-" # os.urandom(16)
+app.config.from_pyfile("./config.py")
 
 def random_string(len=12):
     '''Generate a random string of LEN length, whose characters may
     be ASCII characters and digits.'''
     candidates = string.ascii_letters + string.digits
     return ''.join(random.choice(candidates) for i in range(len))
-
-# TODO moved into some config
-default_erd_image = 'default.png'
-default_erd_source = '''
-title {label: "nfldb Entity-Relationship diagram (condensed)", size: "20"}
-
-# Nice colors from Erwiz:
-# red #fcecec
-# blue #ececfc
-# green #d0e0d0
-# yellow #fbfbdb
-# orange #eee0a0
-
-# Entities
-
-[player] {bgcolor: "#d0e0d0"}
-  *player_id {label: "varchar, not null"}
-  full_name {label: "varchar, null"}
-  team {label: "varchar, not null"}
-  position {label: "player_pos, not null"}
-  status {label: "player_status, not null"}
-
-[team] {bgcolor: "#d0e0d0"}
-  *team_id {label: "varchar, not null"}
-  city {label: "varchar, not null"}
-  name {label: "varchar, not null"}
-
-[game] {bgcolor: "#ececfc"}
-  *gsis_id {label: "gameid, not null"}
-  start_time {label: "utctime, not null"}
-  week {label: "usmallint, not null"}
-  season_year {label: "usmallint, not null"}
-  season_type {label: "season_phase, not null"}
-  finished {label: "boolean, not null"}
-  home_team {label: "varchar, not null"}
-  home_score {label: "usmallint, not null"}
-  away_team {label: "varchar, not null"}
-  away_score {label: "usmallint, not null"}
-
-[drive] {bgcolor: "#ececfc"}
-  *+gsis_id {label: "gameid, not null"}
-  *drive_id {label: "usmallint, not null"}
-  start_field {label: "field_pos, null"}
-  start_time {label: "game_time, not null"}
-  end_field {label: "field_pos, null"}
-  end_time {label: "game_time, not null"}
-  pos_team {label: "varchar, not null"}
-  pos_time {label: "pos_period, null"}
-
-[play] {bgcolor: "#ececfc"}
-  *+gsis_id {label: "gameid, not null"}
-  *+drive_id {label: "usmallint, not null"}
-  *play_id {label: "usmallint, not null"}
-  time {label: "game_time, not null"}
-  pos_team {label: "varchar, not null"}
-  yardline {label: "field_pos, null"}
-  down {label: "smallint, null"}
-  yards_to_go {label: "smallint, null"}
-
-[play_player] {bgcolor: "#ececfc"}
-  *+gsis_id {label: "gameid, not null"}
-  *+drive_id {label: "usmallint, not null"}
-  *+play_id {label: "usmallint, not null"}
-  *+player_id {label: "varchar, not null"}
-  team {label: "varchar, not null"}
-
-[meta] {bgcolor: "#fcecec"}
-  version {label: "smallint, null"}
-  season_type {label: "season_phase, null"}
-  season_year {label: "usmallint, null"}
-  week {label: "usmallint, null"}
-
-# Relationships
-
-player      *--1 team
-game        *--1 team {label: "home"}
-game        *--1 team {label: "away"}
-drive       *--1 team
-play        *--1 team
-play_player *--1 team
-
-game        1--* drive
-game        1--* play
-game        1--* play_player
-
-drive       1--* play
-drive       1--* play_player
-
-play        1--* play_player
-
-player      1--* play_player
-'''
 
 # copied from zapier transformer
 class APIError(Exception):
@@ -132,7 +40,7 @@ class APIError(Exception):
         return rv
 
 def gen_image(source_code, image_path):
-    pobj = subprocess.Popen([os.path.expanduser("~/.cabal/bin/erd"), "--fmt=png", "--output=" + image_path],
+    pobj = subprocess.Popen([os.path.expanduser(app.config["ERD_BIN_PATH"]), "--fmt=png", "--output=" + image_path],
                             stdin=subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             #stdout=subprocess.PIPE
@@ -201,8 +109,8 @@ def erd(userid):
             sourceCode = session["sourceCode"]
             imageFileName = userid + ".png"
         else:
-            sourceCode = default_erd_source
-            imageFileName = default_erd_image
+            sourceCode = app.config["DEFAULT_ERD_SOURCE"]
+            imageFileName = app.config["DEFAULT_ERD_IMAGE"]
 
         obj = {
             "id": userid,
